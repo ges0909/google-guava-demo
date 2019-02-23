965c35716aeb11fbe206bddd264417204eb4779b
Gerrit Schrader <gerrit.schrader@gmail.com>
2019-02-23 12:13:18 +0100

    add filter and transform tests

--- src/test/java/de/gerritschrader/GuavaCollectionTests.java
+++ src/test/java/de/gerritschrader/GuavaCollectionTests.java
@@ -1,6 +1,11 @@
 package de.gerritschrader;
 
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.common.collect.*;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.Test;
 
 import java.util.Collection;
@@ -9,56 +14,57 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class GuavaCollectionTests {
 
     @Test
-    public void list() {
-        List<Number> list = Lists.newArrayList();
+    public void newArrayList() {
+        final List<Number> list = Lists.newArrayList();
         assertThat(list).isNotNull().isEmpty();
     }
 
     @Test
-    public void set() {
-        Set<Number> set = Sets.newHashSet();
+    public void newHashSet() {
+        final Set<Number> set = Sets.newHashSet();
         assertThat(set).isNotNull().isEmpty();
     }
 
     @Test
-    public void map() {
-        Map<Number, String> map = Maps.newHashMap();
+    public void newHashMap() {
+        final Map<Number, String> map = Maps.newHashMap();
         assertThat(map).isNotNull().isEmpty();
     }
 
     @Test
     // create immutable List from a standard collection
     public void immutableList() {
-        List<Number> list = Lists.newArrayList();
-        ImmutableList<Number> immutableList = ImmutableList.copyOf(list);
+        final List<Number> list = Lists.newArrayList();
+        final ImmutableList<Number> immutableList = ImmutableList.copyOf(list);
         assertThat(immutableList).isNotNull().isEmpty();
     }
 
     @Test
     // create immutable Set from a standard collection
     public void immutableSet() {
-        Set<Number> set = Sets.newHashSet();
-        ImmutableSet<Number> immutableSet = ImmutableSet.copyOf(set);
+        final Set<Number> set = Sets.newHashSet();
+        final ImmutableSet<Number> immutableSet = ImmutableSet.copyOf(set);
         assertThat(immutableSet).isNotNull().isEmpty();
     }
 
     @Test
     // create immutable Map from a standard collection
     public void immutableMap() {
-        Map<Number, String> map = Maps.newHashMap();
-        ImmutableMap<Number, String> immutableMap = ImmutableMap.copyOf(map);
+        final Map<Number, String> map = Maps.newHashMap();
+        final ImmutableMap<Number, String> immutableMap = ImmutableMap.copyOf(map);
         assertThat(immutableMap).isNotNull().isEmpty();
     }
 
     @Test
     public void addIterableToCollection() {
-        String[] arr = {"uno", "due", "tre"};
-        Iterable<String> iter = Lists.newArrayList(arr);
-        Collection<String> collection = Lists.newArrayList();
+        final String[] arr = {"uno", "due", "tre"};
+        final Iterable<String> iter = Lists.newArrayList(arr);
+        final Collection<String> collection = Lists.newArrayList();
         Iterables.addAll(collection, iter);
         assertThat(collection).isNotNull().isNotEmpty().hasSize(arr.length);
     }
@@ -66,51 +72,99 @@ public class GuavaCollectionTests {
     @Test
     // check if collection contains element(s) according to a custom matching rule
     public void anyWithCustomMatcher() {
-        Iterable<String> collection = Lists.newArrayList("a", "bc", "def");
+        final Iterable<String> collection = Lists.newArrayList("a", "bc", "def");
         // 'any' returns 'true' if any element satisfies the predicate
-        boolean containsAny = Iterables.any(collection, elem -> elem != null && elem.length() == 1);
+        final boolean containsAny = Iterables.any(collection, elem -> elem != null && elem.length() == 1);
         assertThat(containsAny).isTrue();
-        boolean containsNot = Iterables.any(collection, "uno"::equals);
+        final boolean containsNot = Iterables.any(collection, "uno"::equals);
         assertThat(containsNot).isFalse();
     }
 
     @Test
     public void findWithCustomMatcher() {
-        Iterable<String> collection = Sets.newHashSet("a", "bc", "def");
-        String findAny = Iterables.find(collection, elem -> elem.length() == 1);
+        final Iterable<String> collection = Sets.newHashSet("a", "bc", "def");
+        final String findAny = Iterables.find(collection, elem -> elem.length() == 1);
         assertThat(findAny).isEqualTo("a");
     }
 
     @Test
     public void findDefaultValue() {
-        String whenNotFound = "";
-        Iterable<String> collection = Sets.newHashSet("a", "bc", "def");
-        String findAny = Iterables.find(collection, elem -> "not found".equals(elem), whenNotFound);
+        final String whenNotFound = "";
+        final Iterable<String> collection = Sets.newHashSet("a", "bc", "def");
+        final String findAny = Iterables.find(collection, elem -> "not found".equals(elem), whenNotFound);
         assertThat(findAny).isEqualTo(whenNotFound);
     }
 
     @Test
-    public void filter() {
-        Iterable<String> collection = Sets.newHashSet("a", "bc", "def");
-        // Predicate<String> predicate = elem -> elem.length() == 1;
-        Iterable<String> resultSet = Iterables.filter(collection, elem -> elem.length() == 1);
+    public void filterWithPredefinedPredicateContainsPattern() {
+        final List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        final Collection<String> result = Collections2.filter(names, Predicates.containsPattern("a"));
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).containsExactlyInAnyOrder("Jane", "Adam");
+        // the result of 'Collections.filter()' is a live view of the original collection;
+        // changes to one will be reflected in the other
+        result.add("Anna");
+        assertThat(names.size()).isEqualTo(5);
+        // 'result' is constrained by the predicate; if we add an element that doesn’t
+        // satisfy that Predicate, an IllegalArgumentException will be thrown
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> result.add("Elvis"));
+    }
+
+    @Test
+    public void filterWithCustomPredicate() {
+        final Iterable<String> collection = Sets.newHashSet("a", "bc", "def");
+        final Predicate<String> predicate = new Predicate<String>() {
+            @Override
+            public boolean apply(@Nullable String input) {
+                return input.length() == 1;
+            }
+        };
+        final Iterable<String> resultSet = Iterables.filter(collection, predicate);
         assertThat(resultSet).isNotEmpty();
     }
 
     @Test
+    public void filterWithMultiplePredicates() {
+        final List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        final Collection<String> result = Collections2.filter(names,
+                Predicates.or(
+                        Predicates.containsPattern("J"),
+                        Predicates.not(Predicates.containsPattern("a"))));
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result).containsExactlyInAnyOrder("John", "Jane", "Tom");
+    }
+
+    @Test
+    public void filterOnlyNonNull() {
+        final List<String> names = Lists.newArrayList("John", null, "Jane", null, "Adam", "Tom");
+        final Collection<String> result = Collections2.filter(names, Predicates.notNull());
+        assertThat(result.size()).isEqualTo(4);
+        assertThat(result).containsExactlyInAnyOrder("John", "Jane", "Adam", "Tom");
+    }
+
+    @Test
+    public void checkIfAllElementsMatchACondition() {
+        final List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        boolean result = Iterables.all(names, Predicates.containsPattern("n|m"));
+        assertThat(result).isTrue();
+        result = Iterables.all(names, Predicates.containsPattern("a"));
+        assertThat(result).isFalse();
+    }
+
+    @Test
     public void casting() {
-        List<Number> list = Lists.newArrayList();
-        List<Integer> castedList = (List<Integer>) (List<? extends Number>) list;
+        final List<Number> list = Lists.newArrayList();
+        final List<Integer> castedList = (List<Integer>) (List<? extends Number>) list;
         assertThat(castedList).isNotNull();
     }
 
     @Test
     public void hashBasedTable() {
         // first name, last name, age
-        Table<String /*row key*/, String /*column key*/, Integer /*cell value*/> table = HashBasedTable.create();
+        final Table<String /*row key*/, String /*column key*/, Integer /*cell value*/> table = HashBasedTable.create();
         table.put("Max", "Mustermann", 44);
         table.put("Biene", "Maya", 4);
-        Integer cellValue = table.get("Biene", "Maya");
+        final Integer cellValue = table.get("Biene", "Maya");
         assertThat(table.size()).isEqualTo(2);
         assertThat(cellValue).isEqualTo(4);
     }
@@ -120,6 +174,76 @@ public class GuavaCollectionTests {
         // if a table is needed whose row keys and the column keys need to be ordered by
         // their natural ordering or by supplying comparators, use a TreeBasedTable, which
         // uses TreeMap internally
-        Table<String, String, Integer> table = HashBasedTable.create();
+        final Table<String, String, Integer> table = HashBasedTable.create();
+    }
+
+    @Test
+    public void transform() {
+        Function<String, Integer> func = new Function<String, Integer>() {
+            @Override
+            public Integer apply(String input) {
+                return input.length();
+            }
+        };
+        List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        Collection<Integer> result = Collections2.transform(names, func);
+        assertThat(result.size()).isEqualTo(4);
+        assertThat(result).contains(4, 4, 4, 3);
+        result.remove(3);
+        assertThat(result.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void createFunctionFromAPredicate() {
+        List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        Collection<Boolean> result =
+                Collections2.transform(names,
+                        Functions.forPredicate(Predicates.containsPattern("m")));
+        assertThat(result.size()).isEqualTo(4);
+        assertThat(result).contains(false, false, true, true);
+    }
+
+    @Test
+    public void transformUsingComposedFunction() {
+        Function<String, Integer> f1 = new Function<String, Integer>() {
+            @Override
+            public Integer apply(String input) {
+                return input.length();
+            }
+        };
+        Function<Integer, Boolean> f2 = new Function<Integer, Boolean>() {
+            @Override
+            public Boolean apply(Integer input) {
+                return input % 2 == 0;
+            }
+        };
+        List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        Collection<Boolean> result =
+                Collections2.transform(names, Functions.compose(f2, f1));
+        assertThat(result.size()).isEqualTo(4);
+        assertThat(result).contains(true, true, true, false);
+    }
+
+    @Test
+    public void combineFilterAndTransfWithFluentIterable() {
+        Predicate<String> predicate = new Predicate<String>() {
+            @Override
+            public boolean apply(String input) {
+                return input.startsWith("A") || input.startsWith("T");
+            }
+        };
+        Function<String, Integer> func = new Function<String, Integer>() {
+            @Override
+            public Integer apply(String input) {
+                return input.length();
+            }
+        };
+        List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        Collection<Integer> result = FluentIterable.from(names)
+                .filter(predicate)
+                .transform(func)
+                .toList();
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).containsExactlyInAnyOrder(4, 3);
     }
 }
